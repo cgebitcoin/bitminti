8dc35bc675a3043749e7f430c2ca6c03aaa8d203
cgebitcoin <cgebitcoin@gmail.com>
2025-12-24 00:16:30 -0500

    Enjou BTC3 Dec 24 2025

---
+++ README.md
@@ -0,0 +1,14 @@
+
+BTC3 - The Next Generation
+=====================================
+BTC3 is a lightweight, sandbox‑style fork of Bitcoin Core that gives developers a fully functional blockchain environment
+while eliminating the practical constraints of the public Bitcoin network.
+The fork starts with a custom genesis block that you mined yourself.
+
+
+License
+-------
+
+BTC3 is released under the terms of the MIT license. See [COPYING](COPYING) for more
+information or see https://opensource.org/license/MIT.
+
---
+++ src/kernel/chain.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2022-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_KERNEL_CHAIN_H
+#define BITCOIN_KERNEL_CHAIN_H
+
+#include<iostream>
+
+class CBlock;
+class CBlockIndex;
+namespace interfaces {
+struct BlockInfo;
+} // namespace interfaces
+
+namespace kernel {
+struct ChainstateRole;
+//! Return data from block index.
+interfaces::BlockInfo MakeBlockInfo(const CBlockIndex* block_index, const CBlock* data = nullptr);
+std::ostream& operator<<(std::ostream& os, const ChainstateRole& role);
+} // namespace kernel
+
+#endif // BITCOIN_KERNEL_CHAIN_H
