5204e885b7c83d8c2b38d7bd9a8e956f0653f980
cgebitcoin <cgebitcoin@gmail.com>
2025-12-25 19:54:38 -0500

    adding updated files

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -185,7 +185,7 @@ public:
     // bits we want, but we should get them updated to support all service bits
     // wanted by any release ASAP to avoid it where possible.
     vSeeds.clear();
-    vSeeds.emplace_back("seed.btc3.gurkan.com."); // Example seed
+    vSeeds.emplace_back("seed.btc3.mrio.xyz."); // Example seed
 
     base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 0);
     base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 5);
